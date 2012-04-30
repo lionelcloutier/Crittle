42cf68032c268cd7574a213a83b43d9a371d17a9
lionelcloutier <mr_longhair@hotmail.com>
2012-04-29 19:52:07 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crittle::Application.config.secret_token = 'ffa5c61d5a3d5f83036f2d6358286df136de013592231df0a626162cd40c08c126a545ede97dd2c903776a88953ecf8f9e5f83034167c1d8831b2121aee6ea24'
